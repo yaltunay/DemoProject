c6bae9a94af8ede8e88fbbd511ea6619f0d2ac63
yaltunay <7838100+yaltunay@users.noreply.github.com>
2024-02-14 13:20:39 +0300

    Demo proje oluşturuldu.

---
+++ Project.Api/Infrastructure/Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Api.Core.Application.Repositories;
+using Project.Api.Core.Application.Services;
+using Project.Api.Infrastructure.Persistence.Contexts;
+using Project.Api.Infrastructure.Persistence.Repositories;
+using Project.Api.Infrastructure.Persistence.Services;
+
+namespace Project.Api.Infrastructure.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static void AddPersistenceServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ProjectDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("ProjectDb"));
+        });
+
+        services.AddScoped(typeof(ICustomerRepository), typeof(CustomerRepository));
+        services.AddScoped(typeof(ICustomerTypeRepository), typeof(CustomerTypeRepository));
+
+        services.AddScoped(typeof(ICustomerService), typeof(CustomerService));
+        services.AddScoped(typeof(ICustomerTypeService), typeof(CustomerTypeService));
+    }
+}
